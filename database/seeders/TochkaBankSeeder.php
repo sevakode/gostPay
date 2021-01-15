32c7ce701312d0a6537fde36c540b895ac05d140
drabbit <acer5742zg@mail.ru>
2021-01-15 19:20:47 +0300

    Update

--- database/seeders/TochkaBankSeeder.php
+++ database/seeders/TochkaBankSeeder.php
@@ -20,8 +20,8 @@ class TochkaBankSeeder extends Seeder
         $token->bankId = 'tmVcABICRaAV5Pp4zH3gl3K8eDX8JoDZ';
         $token->bankSecret = 'weZwcRuvBjDyFHtkpAgKVcyV81J1Z4LV';
 
-        $token->accessToken = 'xyJ6jIPOUqAAOmnZ4QWBQ90KkEnVg7jf';
-        $token->refreshToken = 'ro0Ya87ZZDju73VKh5VazwxN4611LtZ9';
+        $token->accessToken = '8z9Ipiz7nLzZI3JNI1DbiltzmVjgGjNP';
+        $token->refreshToken = 'moHrQt6jnZ9wJwoIGLmOoX24u6N0qfqB';
 
         $token->jwtToken = 'ew0KICJ0eXAiOiAiSldUIiwNCiAiYWxnIjogIm5vbmUiDQp9.eyJpc3MiOiJ0b2Noa2EiLCJyZXNwb25zZV90eXBlIjoiY29kZSBpZF90b2tlbiIsImNsaWVudF9pZCI6InRtVmNBQklDUmFBVjVQcDR6SDNnbDNLOGVEWDhKb0RaIiwicmVkaXJlY3RfdXJpIjoiaHR0cHM6Ly9hcHAuZ29zdC5hZ2VuY3kvYXBpL3RhdXRoIiwic3RhdGUiOiJWdWlodnNkcyIsInNjb3BlIjoiYWNjb3VudHMgY2FyZHMgY3VzdG9tZXJzIHNicCBwYXltZW50cyIsIm5vbmNlIjoic2Rmc2ZkcyIsIm1heF9hZ2UiOiI4NjQwMCIsImNsYWltcyI6eyJ1c2VyaW5mbyI6eyJvcGVuYmFua2luZ19pbnRlbnRfaWQiOnsidmFsdWUiOiI0NDAwN2U0NC01MWY3LTQ3NzMtOTU3MC02YzdiYzM1NmQzYzkiLCJlc3NlbnRpYWwiOnRydWV9fSwiaWRfdG9rZW4iOnsib3BlbmJhbmtpbmdfaW50ZW50X2lkIjp7InZhbHVlIjoiNDQwMDdlNDQtNTFmNy00NzczLTk1NzAtNmM3YmMzNTZkM2M5IiwiZXNzZW50aWFsIjp0cnVlfSwiYWNyIjp7InZhbHVlcyI6WyJ1cm46cnViYW5raW5nOnNjYSIsInVybjpydWJhbmtpbmc6Y2EiXSwiZXNzZW50aWFsIjp0cnVlfX19LCJhdWQiOiJodHRwczovL2VudGVyLnRvY2hrYS5jb20ifQ';
 
