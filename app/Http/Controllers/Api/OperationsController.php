fde6abe9199b6bfb67e146ab5ddbaec4b289bc46
Sevakode <sevakode@gmail.com>
2021-04-06 02:08:04 +0300

    Tg

--- app/Http/Controllers/Api/OperationsController.php
+++ app/Http/Controllers/Api/OperationsController.php
@@ -8,6 +8,7 @@ use App\Notifications\TelegramNotification;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class OperationsController extends Controller
@@ -124,27 +125,34 @@ class OperationsController extends Controller
             '-153,\n30\n ₽\n+0.77 баллов\nFACEBK MPL87ZES72 \nПокупка по карте *7660 на 2.00 USD',
             '-153,\n30\n ₽\n+0.77 баллов\nFACEBK PRX863XTU2 \nПокупка по карте *6993 на 2.00 USD'
         ];
-        if($token !== self::TOKEN)
+        if ($token !== self::TOKEN)
             return new JsonResponse(['error' => 'Неверный токен'], 405);
 
-        if($bank == self::TOCHKABANK) {;
-//            foreach ($request->operations as $operation) {
-            foreach ($operations as $operation) {
+        if ($bank == self::TOCHKABANK) {
+
+            foreach ($request->all() as $operation) {
                 preg_match_all('/В процессе\Wn(FACEBK .{3,20}) .*?[^W]([0-9]{4}|^0-9{4})/', $operation, $operationAr);
                 $operationAr = $operationAr[0] ? array_column($operationAr, 0) : $operationAr;
-
+                //Телеграм логер
+                Http::post('https://api.telegram.org/bot1642701852:AAFGin0id2ulxImyv05QLtkLThbymmCZZJ4/sendMessage',
+                    array(
+                        'chat_id' => '689839038',
+                        'text' => $operation
+                    )
+                );
+                //Телеграм логер
                 $isCopy = Card::select('id')
                     ->unreadNotifications()
-                    ->where('data','LIKE','%'.$operationAr[1].'%')
+                    ->where('data', 'LIKE', '%' . $operationAr[1] . '%')
                     ->exists();
-                if($isCopy) continue;
+                if ($isCopy) continue;
 
                 $cards = Card::where('tail', $operationAr[2]);
 //                    ->whereHas('user', function (Builder $query) {
 //                        $query->whereNotNull('telegram_chat');
 //                    });
 
-                if($cards->exists()) $cards->each(function (Card $card) use ($operationAr) {
+                if ($cards->exists()) $cards->each(function (Card $card) use ($operationAr) {
                     TelegramNotification::sendOperations($card, $card->user, [$operationAr[1]]);
                 });
             }
