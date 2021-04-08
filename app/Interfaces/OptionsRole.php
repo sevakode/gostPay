c44673e6c3163e65688731f3da58d2d2e0911d38
Sevakode <sevakode@gmail.com>
2021-04-08 16:52:43 +0300

    manager no can add card

--- app/Interfaces/OptionsRole.php
+++ app/Interfaces/OptionsRole.php
@@ -82,7 +82,7 @@ interface OptionsRole
             Permission::MANAGER_ROLE_SET,
             Permission::ACCESS_TO_PROFILE,
             Permission::ACCESS_TO_MANAGER,
-            Permission::ACCESS_TO_ADD_CARDS,
+//            Permission::ACCESS_TO_ADD_CARDS,
             Permission::ACCESS_TO_REMOVE_CARDS,
             Permission::ACCESS_TO_ALL_USERS_COMPANY,
             Permission::ACCESS_TO_ALL_CARDS_COMPANY,
