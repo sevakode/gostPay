ec0aa9fefc98c3d4f6ec66d5eeb08706ad2e139a
drabbit <acer5742zg@mail.ru>
2021-04-23 02:27:17 +0300

    fix and update

--- resources/views/quick-panel/item-payment-list.blade.php
+++ resources/views/quick-panel/item-payment-list.blade.php
@@ -1,8 +1,6 @@
 <div class="tab-pane fade show pt-3 pr-5 mr-n5 active" id="{{ $id }}" role="tabpanel">
     {{-- Section --}}
     <div class="mb-15">
-{{--        <h5 class="font-weight-bold mb-5">System Messages</h5>--}}
-        {{-- Item --}}
         @foreach($payments as $payment)
         <div class="d-flex align-items-center flex-wrap mb-5">
             <div class="symbol symbol-50 symbol-light mr-5">
@@ -14,7 +12,8 @@
                 <a href="#" class="font-weight-bolder text-dark-75 text-hover-primary font-size-lg mb-1">
                     {{ $title ?? $payment->number() ?? ''}}
                 </a>
-                <span class="text-muted font-weight-bold">{{ $payment->description }}</span>
+                <span class="text-muted font-weight-bold">{{ $payment->description ?: 'Нет данных' }}</span>
+                <span class="text-muted">{{ $payment->operationAt }}</span>
             </div>
             <span class="btn btn-sm btn-light font-weight-bolder py-1 my-lg-0 my-2 text-dark-50">
                 {{ $payment->type == \App\Models\Bank\Payment::EXPENDITURE ? '-' : '+' }}
