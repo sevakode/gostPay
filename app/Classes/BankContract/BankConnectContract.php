90dc46efbe55ccdaee5ea243b409c8377063f5e7
admin <acer5742zg@gmail.com>
2021-11-22 12:42:50 +0300

    fix for php v 7.4

--- app/Classes/BankContract/BankConnectContract.php
+++ app/Classes/BankContract/BankConnectContract.php
@@ -5,5 +5,5 @@ namespace App\Classes\BankContract;
 interface BankConnectContract
 {
     public function connectTokenCredentials(): mixed;
-    public function connectTokenRefresh(): \GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response;
+    public function connectTokenRefresh(): \Illuminate\Http\Client\Response;
 }
