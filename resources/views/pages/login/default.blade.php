5f461ec635833d226f273c5705c0ec14407dadd9
drabbit <acer5742zg@mail.ru>
2022-01-14 17:56:29 +0300

    Base

--- resources/views/pages/login/default.blade.php
+++ resources/views/pages/login/default.blade.php
@@ -1,29 +1,32 @@
-<!--begin::Main-->
-@yield('content')
-<!--end::Main-->
+{{-- Extends layout --}}
+@extends('layout.main')
 
-<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
-<!--begin::Global Config(global config for global JS scripts)-->
-<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
-<!--end::Global Config-->
-@foreach (Metronic::initScripts() as $script) <script src="{{asset($script)}}"></script> @endforeach
-<script src="{{asset('plugins/global/plugins.bundle.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
-<script src="{{asset('js/scripts.bundle.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
-<script>"use strict"; KTApp.block('body', {}); </script>
-@foreach (Metronic::initScripts() as $script)
-    <script src="{{asset($script)}}" charset="ISO-8859-1" type="text/javascript"></script>
-@endforeach
-<!--begin::Global Theme Bundle(used by all pages)-->
-<script src="{{asset('plugins/global/plugins.bundle.js')}}"></script>
+{{-- Content --}}
+@section('body')
+    <body {{ Metronic::printAttrs('body') }} {{ Metronic::printClasses('body') }}>
 
-<script src="{{asset('plugins/custom/prismjs/prismjs.bundle.js')}}"></script>
-<script src="{{asset('js/scripts.bundle.js')}}"></script>
-<!--end::Global Theme Bundle-->
-<!--begin::Page Scripts(used by this page)-->
-<script src="{{asset('js/pages/custom/login/login-general.js')}}"></script>
+    <!--begin::Main-->
+    @yield('content')
+    <!--end::Main-->
 
-<!--end::Page Scripts-->
-@yield('scripts')
-<script charset="ISO-8859-1" type="text/javascript"> KTApp.unblock('body', {}); </script>
-</body>
+    <script charset="ISO-8859-1" type="text/javascript">var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+    <!--begin::Global Config(global config for global JS scripts)-->
+    <script charset="ISO-8859-1" type="text/javascript">var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
+    <!--end::Global Config-->
+    @foreach (Metronic::initScripts() as $script)
+        <script src="{{asset($script)}}" charset="ISO-8859-1" type="text/javascript"></script>
+    @endforeach
+    <!--begin::Global Theme Bundle(used by all pages)-->
+    <script src="{{asset('plugins/global/plugins.bundle.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
+    <script src="{{asset('plugins/custom/prismjs/prismjs.bundle.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
+    <script src="{{asset('js/scripts.bundle.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
+    <script charset="ISO-8859-1" type="text/javascript"> KTApp.block('body', {}); </script>
+    <!--end::Global Theme Bundle-->
+    <!--begin::Page Scripts(used by this page)-->
+    <script src="{{asset('js/pages/custom/login/login-general.js')}}" charset="ISO-8859-1" type="text/javascript"></script>
+
+    <!--end::Page Scripts-->
+    @yield('scripts')
+    <script charset="ISO-8859-1" type="text/javascript"> KTApp.unblock('body', {}); </script>
+    </body>
 @endsection
