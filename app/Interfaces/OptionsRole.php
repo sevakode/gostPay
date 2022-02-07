53c7321b09727a8d2194f7dd23a1bc0f30a64ddb
drabbit <acer5742zg@mail.ru>
2022-02-07 13:44:58 +0300

    fix close card

--- app/Interfaces/OptionsRole.php
+++ app/Interfaces/OptionsRole.php
@@ -169,7 +169,7 @@ interface OptionsRole
             OptionsPermissions::ACCESS_TO_SHOW_BALANCE_FOR_COMPANY ,
             OptionsPermissions::ACCESS_TO_SHOW_BALANCE_FOR_COMPANY_USERS ,
 
-//            OptionsPermissions::ACCESS_TO_CLOSE_CARDS,
+            OptionsPermissions::ACCESS_TO_CLOSE_CARDS,
 //            OptionsPermissions::ACCESS_TO_REMOVE_CARDS,
         ],
     ],
